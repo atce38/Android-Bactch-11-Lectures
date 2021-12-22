4f801155e7ae81bfdde592f2932abd44d94878dc
atce38 <81683047+atce38@users.noreply.github.com>
2021-12-22 12:56:39 +0500

    Setting Text By Java

--- app/src/main/java/com/abidingtech/www/androidbactch11/MainActivity.java
+++ app/src/main/java/com/abidingtech/www/androidbactch11/MainActivity.java
@@ -3,6 +3,8 @@ package com.abidingtech.www.androidbactch11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView hassan=findViewById(R.id.txtHassan);
+        hassan.setText("New Hassan Afzal");
+
+    }
+    void abc(){
 
     }
 }
